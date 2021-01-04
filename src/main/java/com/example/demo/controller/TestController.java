aa3f4325be98aae47d7f42855d44aab7ee62f7db
ikedasoshi <ikeda0soshi@gmail.com>
2021-01-04 15:23:01 +0900

    0104

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -2,11 +2,13 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
-    @RequestMapping("/counter_test")
-    public String index() {
-        return "counter_test";
+    @RequestMapping(value = "/counter_test", method = RequestMethod.GET)
+    public ModelAndView countertest(ModelAndView mav) {
+        mav.setViewName("counter_test");
+        return mav;
     }
 }
