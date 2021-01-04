39530170c363bb000862ebb4b52b935e8df8c7b5
ikedasoshi <ikeda0soshi@gmail.com>
2021-01-04 16:14:48 +0900

    0104

---
+++ src/main/java/com/example/demo/controller/DataObjectController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class DataObjectController {
+    
+}
+//page138
+class DataObject {
+    private int id;
+    private String name;
+    private String value;
+}
