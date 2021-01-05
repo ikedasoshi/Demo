40668293cdea65f481d16cb393be5f071a33623a
ikedasoshi <ikeda0soshi@gmail.com>
2021-01-05 13:49:24 +0900

    0105

---
+++ src/main/java/com/example/demo/controller/IfSampleController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IfSampleController {
+    @RequestMapping("/ifsample/{id}")
+    public ModelAndView ifsample(@PathVariable int id, ModelAndView mav) {
+        mav.setViewName("chapter4/ifsample");
+        mav.addObject("id", id);
+        mav.addObject("check", id % 2 == 0);
+        mav.addObject("trueVal", "Even number");
+        mav.addObject("falseVal", "Odd number");
+        return mav;
+    }
+}
