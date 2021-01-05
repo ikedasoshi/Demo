ac3cc97f1dd69d61ff0e09cdc461db0a662f88c4
ikedasoshi <ikeda0soshi@gmail.com>
2021-01-05 09:46:03 +0900

    0105

--- src/main/java/com/example/demo/controller/DataObjectController.java
+++ src/main/java/com/example/demo/controller/DataObjectController.java
@@ -1,18 +1,53 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class DataObjectController {
-    
+    @RequestMapping("/dotest")
+    public ModelAndView dotest(ModelAndView mav) {
+        mav.setViewName("chapter4/dotest");
+        mav.addObject("msg", "current data.");
+        DataObject obj = new DataObject(123, "hanako", "hanako@flower");
+        mav.addObject("object", obj);
+        return mav;
+    }
 }
-//page138
+
 class DataObject {
     private int id;
     private String name;
     private String value;
+
+    public DataObject(int id, String name, String value) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+    public void setValue(String value) {
+        this.value = value;
+    }
+
 }
