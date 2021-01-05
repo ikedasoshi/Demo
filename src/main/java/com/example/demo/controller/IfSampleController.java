cd51267769c43aa2df7a8bc9cfd812892f8bf858
ikedasoshi <ikeda0soshi@gmail.com>
2021-01-05 16:10:53 +0900

    0105

--- src/main/java/com/example/demo/controller/IfSampleController.java
+++ src/main/java/com/example/demo/controller/IfSampleController.java
@@ -25,4 +25,14 @@ public class IfSampleController {
         mav.addObject("falseVal", "Negative");
         return mav;
     }
+
+    @RequestMapping("/thswitch/{month}")
+    public ModelAndView thswitch(@PathVariable int month, ModelAndView mav) {
+        mav.setViewName("chapter4/thswitch");
+        int m = Math.abs(month) % 12;
+        m = m == 0 ? 12 : m;
+        mav.addObject("month", m);
+        mav.addObject("check", Math.floor(m / 3));
+        return mav;
+    }
 }
