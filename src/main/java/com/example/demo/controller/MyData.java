421af61e1c8dcccbcb2da203506d3eea3ec01b02
ikedasoshi <ikeda0soshi@gmail.com>
2021-01-12 15:54:50 +0900

    0112

--- src/main/java/com/example/demo/controller/MyData.java
+++ src/main/java/com/example/demo/controller/MyData.java
@@ -2,6 +2,21 @@ package com.example.demo.controller;
 
 import javax.persistence.*;
 
+@Entity
+@Table(name = "mydata")
 public class MyData {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    
+    @Column
+    private long id;
+    @Column(length = 50, nullable = false)
+    private String name;
+    @Column(length = 200, nullable = true)
+    private String mail;
+    @Column(nullable = true)
+    private Integer age;
+    @Column(nullable = true)
+    private String memo;
 }
