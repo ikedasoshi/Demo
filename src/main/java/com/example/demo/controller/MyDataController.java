6da71a792e799baf012a40fd0fcf1871308ab876
ikedasoshi <ikeda0soshi@gmail.com>
2021-01-13 16:16:41 +0900

    0113

--- src/main/java/com/example/demo/controller/MyDataController.java
+++ src/main/java/com/example/demo/controller/MyDataController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,12 +14,19 @@ public class MyDataController {
     @Autowired
     MyDataRepository repository;
 
-    @RequestMapping("/mydatatest")
-    public ModelAndView mydatatest(ModelAndView mav) {
+    @RequestMapping(value = "/mydatatest", method = RequestMethod.GET)
+    public ModelAndView mydatatest(@ModelAttribute("formModel") MyData mydata, ModelAndView mav) {
         mav.setViewName("chapter5/mydatatest");
         mav.addObject("msg", "this is sample comment");
         Iterable<MyData> list = repository.findAll();
-        mav.addObject("data", list);
+        mav.addObject("datalist", list);
         return mav;
+    }
+
+    @RequestMapping(value = "/mydatatest", method = RequestMethod.POST)
+    @Transactional(readOnly = false)
+    public ModelAndView form(@ModelAttribute("formModel") MyData mydata, ModelAndView mav) {
+        repository.saveAndFlush(mydata);
+        return new ModelAndView("redirect:/chapter5/mydatatest");
     }
 }
