f8a22205876416f789c76272cc9ed991d533745c
ikedasoshi <ikeda0soshi@gmail.com>
2021-01-15 16:12:33 +0900

    0115

---
+++ src/main/java/com/example/demo/controller/DeleteController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.controller.repositories.MyDataRepository;
+
+@Controller
+public class DeleteController {
+    
+}
