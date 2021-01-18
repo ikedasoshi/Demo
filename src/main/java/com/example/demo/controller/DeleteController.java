38d99c47b4164863600df50c8fc8121d97f07806
ikedasoshi <ikeda0soshi@gmail.com>
2021-01-18 15:57:49 +0900

    0118

--- src/main/java/com/example/demo/controller/DeleteController.java
+++ src/main/java/com/example/demo/controller/DeleteController.java
@@ -13,4 +13,24 @@ import com.example.demo.controller.repositories.MyDataRepository;
 @Controller
 public class DeleteController {
     
+    @Autowired
+    MyDataRepository repository;
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public ModelAndView delete(@PathVariable int id, ModelAndView mav) {
+        mav.setViewName("chapter5/delete");
+        mav.addObject("title", "delete mydata.");
+        Iterable<MyData> list = repository.findAll();
+        mav.addObject("datalist", list);
+        Optional<MyData> data = repository.findById((long)id);
+        mav.addObject("formModel", data.get());
+        return mav;
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    @Transactional(readOnly = false)
+    public ModelAndView remove(@RequestParam long id, ModelAndView mav) {
+        repository.deleteById(id);
+        return new ModelAndView("redirect:/mydatatest");
+    }
 }
