c7cf2fd9bfb7dc77fff7363ba5d42f9ef4caaa4c
ikedasoshi <ikeda0soshi@gmail.com>
2021-01-27 16:16:24 +0900

    0127

--- src/main/java/com/example/demo/controller/simulator/S_Simulator_Controller.java
+++ src/main/java/com/example/demo/controller/simulator/S_Simulator_Controller.java
@@ -1,5 +1,50 @@
 package com.example.demo.controller.simulator;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.*;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+//import org.springframework.transaction.annotation.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.controller.simulator.SimuData;
+import com.example.demo.controller.repositories.SimuDataRepository;
+
+@Controller
 public class S_Simulator_Controller {
     
+    @Autowired
+    SimuDataRepository repository;
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    SimuDataDAOImpl dao;
+
+    @PostConstruct
+    public void init() {
+        dao = new SimuDataDAOImpl(entityManager);
+    }
+
+    @RequestMapping(value = "/simulator", method = RequestMethod.GET)
+    public ModelAndView select(HttpServletRequest request, ModelAndView mav) {
+        mav.setViewName("simulator/simulator");
+        mav.addObject("title", "Find Page");
+        mav.addObject("msg", "this is MyData sample");
+        mav.addObject("value", "");
+        Iterable<SimuData> list = dao.getAll();
+        mav.addObject("datalist", list);
+        return mav;
+    }
+
+    @RequestMapping(value = "/simulator", method = RequestMethod.POST)
+    public ModelAndView simulator(HttpServletRequest request, ModelAndView mav) {
+        
+        return mav;
+    }
 }
--- src/main/java/com/example/demo/controller/simulator/SimuDataDAOImpl.java
+++ src/main/java/com/example/demo/controller/simulator/SimuDataDAOImpl.java
@@ -30,4 +30,15 @@ public class SimuDataDAOImpl implements SimuDataDAO<SimuData> {
         entityManager.close();
         return list;
     }
+
+    @Override
+    public SimuData findById(long id) {
+        return (SimuData)entityManager.createQuery("from MyData where id = " + id).getSingleResult();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<SimuData> findByName(String name) {
+        return (List<SimuData>)entityManager.createQuery("from MyData where name = " + name).getResultList();
+    }
 }
