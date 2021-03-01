686f829c486d9fa9f3aa42c55aaee98f1f53f05b
ikedasoshi <ikeda0soshi@gmail.com>
2021-03-01 14:50:26 +0900

    0301

--- src/main/java/com/example/demo/controller/bbps4/S_Simulator_Controller.java
+++ src/main/java/com/example/demo/controller/bbps4/S_Simulator_Controller.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.controller.bbps4.SimuData;
 import com.example.demo.controller.repositories.SimuDataRepository;
